b0fdd59c6b92fea80ff858a181c602877ceab618
ei1903 <ei1903@hamako-ths.ed.jp>
2021-11-07 10:41:41 +0900

    debug

--- HOJ/contestMenuUtils.user.js
+++ HOJ/contestMenuUtils.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         HOJ-ContestMenuUtils
-// @version      0.1
+// @version      0.2
 // @discription  HOJのコンテスト画面でのメニューを便利に
 // @author       ei1903
 // @updateURL    
@@ -10,6 +10,8 @@
 (function() {
     var menu = document.getElementsByTagName('ul')[0];
     menu.childNodes.forEach((node) => {
-        console.log(node.nodeName);
+        if (node.firstChild.nodeName == 'A' && node.firstChild.href == location.href) {
+            console.log(node.firstChild.href);
+        }
     });
 })();
